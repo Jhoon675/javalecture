f2fadf3eca054ec02a85cc57ee6b4c0910b10d58
JongHoon <kjh96120@naver.com>
2020-09-28 13:43:14 +0900

    first commit

---
+++ src/bit/r45/first/Hello.java
@@ -0,0 +1,39 @@
+package bit.r45.first;
+
+import java.io.IOException;
+import bit.r45.members.*;
+import bit.r45.first.Student; 
+
+public class Hello 
+{	
+	public static void main(String[] args) 
+	{
+		/* 
+		 * // 이름이 같은 파일을 쓸때는 헷갈리지 않도록 풀네임을 명시해준다. bit.r45.members.Student student;
+		 * bit.r45.first.Student student1;
+		 * 
+		 * student = new Student();
+		 * 
+		 * // try { // System.in.read(); // } catch (IOException e) { // // TODO
+		 * Auto-generated catch block // e.printStackTrace(); // }
+		 * 
+		 * System.out.println("헬로~~~~~");
+		 * 
+		 * System.err.println("에러!!!");
+		 */
+		 
+		Student student; // 자료형을 선언
+		
+		student = new Student(); // class는 new를 사용하여 할당한다.
+		student.run();
+		student.setAge(100);
+		student.run();
+		
+	}
+
+	
+	
+	
+	
+	
+}
---
+++ src/bit/r45/members/Student.java
@@ -0,0 +1,41 @@
+package bit.r45.members;
+
+// 클래스
+public class Student {
+	//전달 요소 String nam, int age
+	
+	// 클래스 안에는 멤버들이 와야 한다.
+	// 멤버들: 필드(속성), 메서드(함수)
+	// 필드의 값이 바뀌는 행위들 = 메서드
+	
+	/* 필드 */
+	int age;
+	int color; // 1: red, 2: blue, 3: balck
+	
+	/* run()이라는 메서드 */
+	public void run() {
+		// 1개의 구문은 1개의 문장만 쓸 수 있다.
+		int x = 1; //변수 x를 선언하고 1을 저장
+		int y = 2; //변수 y를 선언하고 2을 저장
+		
+		int result = x + y;
+//		int
+//		result
+//		= x + y;
+		// 여러줄에 걸쳐서 작성해도 상관은 없으나 2개 이상의 구문이 되면 안된다.
+		
+		System.out.println(x + "+" + y + "=" + result);
+		System.out.println("Student AGE: " + this.age);
+	}
+	
+	
+	// this
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
+	
+	
+	
+}
